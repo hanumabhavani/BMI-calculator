5ebe7048a377e5e74d9d290f86ee7ba05eeb1983
hanumabhavani <103923527+hanumabhavani@users.noreply.github.com>
2022-06-28 10:43:30 +0530

    Add files via upload

---
+++ BMIcalculator.py
@@ -0,0 +1,7 @@
+# to calculate Body Mass Index(BMI) 
+height = input('enter your height in m: ')
+weight = input('enter your weight in kg: ')
+height=float(height)
+weight=int(weight)
+BMI = weight//height**2
+print('your BMI is ',BMI)
